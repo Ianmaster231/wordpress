6d02a48cccd3d1f5abac6096ff578d6033248f5c
Ian Zammit <ian.zammit.a100896@mcast.edu.mt>
2022-11-18 12:57:24 +0100

    basic function excersise

--- app/public/wp-content/themes/Ian-Theme/index.php
+++ app/public/wp-content/themes/Ian-Theme/index.php
@@ -1,1 +1,17 @@
-This is ian amazing custom theme.
+<?php 
+
+   // function myFirstFunction(){
+        //echo 2+2; number output
+    //    echo "<p>Hello, this is a string output</p>";
+    //}
+
+    //myFirstFunction();
+
+  //  function greet($name, $food){
+   //     echo "<p>Hi,my name is $name and my favourite color is $food.</p>";
+  //  }
+  //  greet('Ian', 'pizza');
+  //  greet('Max', 'burger');
+?>
+<h1><?php bloginfo('name'); ?></h1>
+    <p><?php bloginfo('description')?></p>
