72583cea31f3871578b3b40336a2c5a40693a4e0
Ian Zammit <ian.zammit.a100896@mcast.edu.mt>
2022-11-18 13:28:15 +0100

    arrays exercise done

---
+++ app/public/wp-content/themes/Ian-Theme/index - Arrays.php
@@ -0,0 +1,17 @@
+<?php 
+
+   // function myFirstFunction(){
+        //echo 2+2; number output
+    //    echo "<p>Hello, this is a string output</p>";
+    //}
+
+    //myFirstFunction();
+
+  //  function greet($name, $food){
+   //     echo "<p>Hi,my name is $name and my favourite color is $food.</p>";
+  //  }
+  //  greet('Ian', 'pizza');
+  //  greet('Max', 'burger');
+?>
+<h1><?php bloginfo('name'); ?></h1>
+    <p><?php bloginfo('description')?></p>
